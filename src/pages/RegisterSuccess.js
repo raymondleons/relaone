7811498233dcca077610f6499fe032ee9c4fb1d7
marbincah <mariabinarc@gmail.com>
2019-03-28 22:55:30 +0700

    [Binar] - add new RegisterSuccess.js and an image

---
+++ src/pages/RegisterSuccess.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Container, Row, Col, Button } from 'reactstrap';
+import bluelogo from '../assets/images/blue-logo.png';
+import registersuccess from '../assets/images/register-success.png';
+import '../assets/css/_style.scss'
+
+class RegisterSuccess extends Component {
+  render() {
+    return (
+      <div className="register-success">
+        <Container>
+            <Row>
+                <div className="logo-center">
+                    <img src={bluelogo} alt="RelaOne Logo"></img>
+                </div>
+            </Row>
+            <Row>
+              <Col md="2"></Col>
+              <Col md="8">
+                <div className="register-success-content">
+                  <img src={registersuccess} alt="check your email" className="icon-success"></img>
+                  <h3>Thank you for registering.</h3>
+                  <p>Please check your inbox and click the link emailed to you.</p>
+                </div>
+              </Col>
+            </Row>
+        </Container>
+    </div>
+    )
+  }
+}
+
+export default RegisterSuccess;
