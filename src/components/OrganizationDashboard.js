007f4e5df86d5e2cb0480aeef0fafb1f463264f3
marbincah <mariabinarc@gmail.com>
2019-03-29 11:41:55 +0700

    [Binar] - create organization dashboard page and styling

--- src/components/OrganizationDashboard.js
+++ src/components/OrganizationDashboard.js
@@ -5,9 +5,11 @@ import '../assets/css/_style.scss'
 class FormOrganizationProfile extends Component {
   render() {
     return (
-      <div className="form-organization-profile">
-        <div className="content-title">
-            <h3>Event</h3>
+      <div className="organization-dashboard">
+        <div className="content-title content-header">
+            <div>
+              <h3>Event</h3>
+            </div>
             <div className="event-count">
                 <h4>0</h4>
                 <p>EVENT FOUND</p>
@@ -18,8 +20,8 @@ class FormOrganizationProfile extends Component {
             <a>Delete</a>
             <Button color="primary">Create Event</Button>
         </div>
-        <div>
-            <img src={} alt="create event"/>
+        <div className="no-event">
+            {/* <img src={} alt="create event"/> */}
             <h3>You have no event yet.</h3>
             <p>Please create an voluntary event to find volunteers.</p>
         </div>
