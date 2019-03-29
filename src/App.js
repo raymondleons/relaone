8ec49013d50e46f9c205edf735be2ec1459be767
marbincah <mariabinarc@gmail.com>
2019-03-29 13:03:08 +0700

    [Binar] - add update user profile success page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import UpdateOrganizationProfile from './pages/UpdateOrganizationProfile';
 import './assets/css/_style.scss';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import UpdateOrgProfileSuccess from './pages/UpdateOrgProfileSuccess';
+import UpdateUserProfileSuccess from './pages/UpdateUserProfileSuccess';
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
           <Route path="/" exact component={LandingPage}/>
           <Route path="/organization/update-profile" exact component={UpdateOrganizationProfile}/>
           <Route path="/organization/update-profile/success" exact component={UpdateOrgProfileSuccess}/>
+          <Route path="/user/update-profile/success" exact component={UpdateUserProfileSuccess}/>
         </div>
       </Router>
     );
