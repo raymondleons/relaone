3dce6b91367f9d548c41ad83ff2ef5eff06591fb
Raymond Leon Sembiring <raymondleons1997@gmail.com>
2019-03-31 22:06:40 +0700

    [Raymond] - finish for new design

--- src/layout/Dashboard.js
+++ src/layout/Dashboard.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
-import Event from '../assets/images/event/banjir.jpg'
-import Rekomendasi from '../assets/images/recomendation.svg'
-import Post from '../assets/images/event/post.jpg'
+import { Container, Row } from 'reactstrap';
+import DashboardPage from '../components/DashboardPage'
+import '../assets/css/_style.scss';
+
 
 
 export default class Dashboard extends Component {
@@ -14,83 +14,11 @@ export default class Dashboard extends Component {
   render() {
     return (
         <div className="form-update-profile">
-        <div>
-            <h3 className="title"><b>Dashboard</b></h3>
+          <Container>
+            <DashboardPage/>
+          </Container>
         </div>
-        <hr></hr>
-        <div>
-            <h5>List Event</h5>
-        </div>
-        <div className="col-md-12">
-            <div className="form-event">
-              <img src={Event} style={style.event} alt=""/>
-              <h5 style={style.title}>Banjir Bandang Terjang Aceh Tenggara</h5>
-              <p style={style.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque quae sequi exercitationem, ea facilis eveniet! </p>
-              <img src={Rekomendasi} style={style.foto} alt=""/>
-              <p style={style.desc}>Rekomendasi</p>
-              <Button color="primary" style={{float:"right", marginTop:"-30px"}}>Join Now</Button>
-            </div>
-        </div>
-        <div>
-            <h5>List Article</h5>
-        </div>
-        <div className="col-md-4">
-          <div className="form-event">
-            <img src={Post} style={style.post} alt=""/>
-            <h5 style={{fontSize:"15px", paddingTop:"10px", textAlign:"center"}}>Cara Menghindari Gempa Dengan Aman</h5>
-            <p style={{fontSize:"12px", paddingTop:"7px"}}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae, architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod aliquam molestiae enim optio rerum? Incidunt animi dolores totam provident numquam.</p>
-            <Button color="primary" style={{marginLeft:"10px"}}>Read more</Button>
-          </div>
-        </div>
-        <div className="col-md-4" style={{float:"right", marginTop:"-577px", marginRight:"230px"}}>
-          <div className="form-event">
-            <img src={Post} style={style.post} alt=""/>
-            <h5 style={{fontSize:"15px", paddingTop:"10px", textAlign:"center"}}>Cara Menghindari Gempa Dengan Aman</h5>
-            <p style={{fontSize:"12px", paddingTop:"7px"}}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae, architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod aliquam molestiae enim optio rerum? Incidunt animi dolores totam provident numquam.</p>
-            <Button color="primary" style={{marginLeft:"10px"}}>Read more</Button>
-          </div>
-        </div>
-        <div className="col-md-4" style={{float:"right", marginTop:"-577px", marginRight:"14px"}}>
-          <div className="form-event">
-            <img src={Post} style={style.post} alt=""/>
-            <h5 style={{fontSize:"15px", paddingTop:"10px", textAlign:"center"}}>Cara Menghindari Gempa Dengan Aman</h5>
-            <p style={{fontSize:"12px", paddingTop:"7px"}}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae, architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod aliquam molestiae enim optio rerum? Incidunt animi dolores totam provident numquam.</p>
-            <Button color="primary" style={{marginLeft:"10px"}}>Read more</Button>
-          </div>
-        </div>
-      </div>
     )
-  }
-}
-
-const style = {
-  event: {
-    width: "200px"
-  },
-  title: {
-    float: "right"
-  },
-  text: {
-    float:"right",
-    marginTop:"-80px",
-    paddingLeft: "216px"
-  },
-  foto: {
-    float:"right",
-    marginRight:"-233px",
-    width:"17px"
-  },
-  desc: {
-    float:"right",
-    fontSize: "10px",
-    color: "gray",
-    marginTop:"-1px",
-    marginRight:"-300px",
-  },
-  post: {
-    marginLeft:"-9px",
-    width:"150px",
-    boxShadow:"1px 1px 10px 1px #888888"
   }
 }
 
