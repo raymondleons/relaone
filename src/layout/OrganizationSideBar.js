83afc5cccd69ad56a4eb5e0476c731e66af0b378
marbincah <mariabinarc@gmail.com>
2019-04-02 09:54:56 +0700

    [Binar] - add url to some element

--- src/layout/OrganizationSideBar.js
+++ src/layout/OrganizationSideBar.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { UncontrolledCollapse } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 class OrganizationSideBar extends Component {
   render() {
     return (
       <div className="side-bar">
         <div className="side-bar-list">
-          <div className="side-bar-item">Event</div>
+          <div className="side-bar-item"><Link to="/event">Event</Link></div>
           <div className="side-bar-item" id="volunteer">Volunteer</div>
             <UncontrolledCollapse toggler="#volunteer">
               <div className="side-bar-subitem">Registered</div>
