3e66a4bdae9f2847892bb41074c3a942c06e6964
marbincah <mariabinarc@gmail.com>
2019-04-03 19:10:51 +0700

    [Binar] - add redux

---
+++ src/reducers/landingPage.js
@@ -0,0 +1,14 @@
+const initialState={
+    intro: 'RelaOne'
+}
+
+const landingPage=(state=initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
+
+export default landingPage;
+
+
