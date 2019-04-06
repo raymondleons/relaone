fd1b884f2f05640da52e7639e3047757ecf6519b
marbincah <mariabinarc@gmail.com>
2019-04-06 14:26:47 +0700

    [Binar] - change file name

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Row, Col, Card, CardImg, CardBody, CardTitle, CardText} from 'reactstrap';
 import Dotdotdot from 'react-dotdotdot';
-import { getArticle } from '../actions' ;
+import { getArticle } from '../actions/userActions' ;
 
 class ArticleList extends Component {
 
