9ebd57ecb5236c7e386d5d2712ca9cdbeb3d97f8
Raymond Leon Sembiring <raymondleons1997@gmail.com>
2019-04-10 21:19:07 +0700

    [Raymond] - adding new menu

--- src/layout/Admin/LeftSidebar.js
+++ src/layout/Admin/LeftSidebar.js
@@ -10,6 +10,30 @@ export default class LeftSidebar extends Component {
                     <li className="submenu">
                         <a href="#" className="active"><i className="fa fa-fw fa-bars"></i><span> Dashboard </span></a>
                     </li>
+                    <li className="submenu">
+                        <a href="#"><i className="fa fa-user"></i> <span> Users </span> <span class="menu-arrow"></span></a>
+                          <ul className="list-unstyled">
+                            <li><a href="tables-basic.html">Add New User</a></li>
+                          </ul>
+                    </li>
+                    <li className="submenu">
+                        <a href="#"><i className="fa fa-book"></i> <span> Article </span> <span class="menu-arrow"></span></a>
+                          <ul className="list-unstyled">
+                            <li><a href="tables-basic.html">Add New Article</a></li>
+                          </ul>
+                    </li>
+                    <li className="submenu">
+                        <a href="#"><i className="fa fa-users"></i> <span> Organization </span> <span class="menu-arrow"></span></a>
+                          <ul className="list-unstyled">
+                            <li><a href="tables-basic.html">Add New Organization</a></li>
+                          </ul>
+                    </li>
+                    <li className="submenu">
+                        <a href="#"><i className="fa fa-user"></i> <span> Events </span> <span class="menu-arrow"></span></a>
+                          <ul className="list-unstyled">
+                            <li><a href="tables-basic.html">Add New Events</a></li>
+                          </ul>
+                    </li>
                 </ul>
             </div>
         </div>
