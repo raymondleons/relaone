e52f512921efc8ce7c45ba4eb46148441d11e007
Raymond Leon Sembiring <raymondleons1997@gmail.com>
2019-04-26 06:12:31 +0700

    react-create-app on Heroku

--- src/pages/LoginRoot.js
+++ src/pages/LoginRoot.js
@@ -26,12 +26,14 @@ export default class RegisterHomeRoot extends Component {
             <div className="d-flex justify-content-around mt-5">
             <Link to='/login'><FontAwesomeIcon icon='user-circle' className="fa-6x"/></Link>
             <Link to='/login-org'><FontAwesomeIcon icon='home' className="fa-6x"/></Link>
+            <Link to='/login-admin'><FontAwesomeIcon icon='user-circle' className="fa-6x"/></Link>
             
             
             </div>
-            <div className="d-flex justify-content-around text-center mt-2 mb-5">
+            <div className="d-flex justify-content-around text-center mt-3 mb-5">
             <p className="para"> Login as Volunteer</p>
             <p className="para">Login as Organization</p>
+            <p className="para">Login as Admin</p>
             </div>
             <div className="mt-5">
               <p className="para text-center">Don't have an account yet ? <Link to='/register'>Register here.</Link></p>
