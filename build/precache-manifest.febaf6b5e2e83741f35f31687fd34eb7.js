587e869e766d5028cf10a1f5aeb00825b845280e
bassrullah <bassrullah@gmail.com>
2019-05-09 13:14:25 +0700

    [Bass] - fix bugs

---
+++ build/precache-manifest.febaf6b5e2e83741f35f31687fd34eb7.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "0aafd904a5713042e40293e1b3824d34",
+    "url": "/static/media/person2.0aafd904.jpeg"
+  },
+  {
+    "revision": "a25fb90ad94ccfe7cec8",
+    "url": "/static/css/main.fc80f4b8.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "c4ad9a63c015e89b5221",
+    "url": "/static/js/2.56b7aef5.chunk.js"
+  },
+  {
+    "revision": "35091df93cc7a5c80c47fe071e8bd981",
+    "url": "/static/media/MavenPro-Medium.35091df9.ttf"
+  },
+  {
+    "revision": "bf7f1be0aff3fb26e73e55d281eff402",
+    "url": "/static/media/find-volunteer.bf7f1be0.png"
+  },
+  {
+    "revision": "2a1e1b7b1b324ecbfc15ad28a8d928e9",
+    "url": "/static/media/background.2a1e1b7b.png"
+  },
+  {
+    "revision": "25932649144ee8eef5de41461b6156ac",
+    "url": "/static/media/bg-contactus.25932649.jpg"
+  },
+  {
+    "revision": "f979fe04853915fb2570794493c05a71",
+    "url": "/static/media/noevent.f979fe04.png"
+  },
+  {
+    "revision": "eaffb4f35861530a1d9b975cceb92f3d",
+    "url": "/static/media/image1.eaffb4f3.png"
+  },
+  {
+    "revision": "5bb43131410096b717bf58d5d9e9e484",
+    "url": "/static/media/certemplate.5bb43131.jpg"
+  },
+  {
+    "revision": "2a9115b13cdb85fa9649e762a73b6437",
+    "url": "/static/media/person1.2a9115b1.jpg"
+  },
+  {
+    "revision": "a25fb90ad94ccfe7cec8",
+    "url": "/static/js/main.583d98df.chunk.js"
+  },
+  {
+    "revision": "ffae3392599365f4812ac2e2cb9174f4",
+    "url": "/static/media/person3.ffae3392.jpg"
+  },
+  {
+    "revision": "4dfb0edc0d28ecff5b1aa8b04a1ed070",
+    "url": "/static/media/phone.4dfb0edc.png"
+  },
+  {
+    "revision": "54f2250d5e44d50f0c4354e21767ac37",
+    "url": "/static/media/inbox.54f2250d.png"
+  },
+  {
+    "revision": "b301d45b5edcac26c4cd30d7d4144309",
+    "url": "/static/media/banjir.b301d45b.jpg"
+  },
+  {
+    "revision": "d00aa944e028e2d7a6ff66cd405d0b0a",
+    "url": "/static/media/lock.d00aa944.png"
+  },
+  {
+    "revision": "9f9909a6a51c9f6367d7dd42289ccfa8",
+    "url": "/static/media/avatar.9f9909a6.png"
+  },
+  {
+    "revision": "3e1695b81154f47e43bff3e5d9b4989a",
+    "url": "/static/media/organization-img.3e1695b8.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "c4ad9a63c015e89b5221",
+    "url": "/static/css/2.022c0af1.chunk.css"
+  },
+  {
+    "revision": "2347f68b52b385d2d53df209c2f5f8b5",
+    "url": "/index.html"
+  }
+];
